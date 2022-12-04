8597d2082eede1e2930768089f79fcd46a528dda
bden <bkdenhartog@gmail.com>
2022-12-04 16:01:03 -0700

    Testing store; Changed app to see Surrender and Intake forms; Intake form sees values from three components SetCatColorX are prototype.  Shared store currently limited to number, want to change to structure.

---
+++ src/components/SetCatColor3.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { catInfo } from './stores.js';
+
+	function changeCatInfo() {
+		catInfo.set( 3);
+	}
+</script>
+
+<button on:click={changeCatInfo}>
+	Set Cat Info to 3
+</button>
---
+++ src/routes/IntakeForm.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import SetCatColor1 from '../components/SetCatColor1.svelte'
+	import SetCatColor2 from '../components/SetCatColor2.svelte'
+	import SetCatColor3 from '../components/SetCatColor3.svelte'
+	import { catInfo } from '../components/stores.js'
+
+	let catInfoValue = false
+	catInfo.subscribe((value: any) => {
+		catInfoValue = value
+	})
+
+	function handleSubmit() {
+		return false // prevent reload
+	}
+
+	let formValid = false
+	function getFormValid() {
+		console.log('valid tested')
+		return true
+	}
+	$: formValid = getFormValid()
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	Stored Cat Info is {catInfoValue}
+	<SetCatColor1 />
+	<SetCatColor2 />
+	<SetCatColor3 />
+</form>
+
+<style>
+</style>
