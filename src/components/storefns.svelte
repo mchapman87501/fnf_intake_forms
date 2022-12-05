8e975cb4564535eefd1f3b41f0703da5f710f8a3
bden <bkdenhartog@gmail.com>
2022-12-04 18:56:56 -0700

    Add file

---
+++ src/components/storefns.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" context="module">
+	import { catInfo, catInfoPkg } from './stores'
+	import { get } from 'svelte/store'
+	
+	export function setOKKinder(val: string) {
+		let temp = get(catInfoPkg)
+		temp.okKinder = val
+		catInfoPkg.set(temp)
+	}
+ 
+</script>
