5c837c641b9793b6296a2d3a24d5969861294a82
bden <bkdenhartog@gmail.com>
2022-12-04 17:28:04 -0700

    Refactoring function setOKKinder to storefns.svelte

--- src/components/SetCatColor3.svelte
+++ src/components/SetCatColor3.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { catInfo } from './stores.js';
+	import { catInfoPkg } from './stores.js';
+	import { setOKKinder} from './storefns.svelte'
 
 	function changeCatInfo() {
 		catInfo.set( 'black');
 	}
+	
+	let myOK = 'No'
 </script>
 
 <button on:click={changeCatInfo}>
 	Set Cat Info to black
+</button>
+<button on:click={() => setOKKinder(myOK)}>
+	Set Cat Kinder to {myOK}
 </button>
