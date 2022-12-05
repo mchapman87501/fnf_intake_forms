4ea88b8b5045ee6c1be2f64c44dfab733d57991a
bden <bkdenhartog@gmail.com>
2022-12-05 10:37:41 -0700

    Move getIntakeInfoAsCSV to UtilsFns.svelte. Accepts String[][] of headers and values.

---
+++ src/components/UtilFns.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" context="module">
+export function getIntakeInfoAsCSV(tableInfo : string[][]): string {
+    function valueToCSV(v: string): string {
+        const quoted = v.replaceAll('"', '""')
+        return `"${quoted}"`
+    }
+
+    function rowToStr2(row: Array<string>): string {
+        return row.map((col) => valueToCSV(col)).join(',')
+    }
+
+    return tableInfo
+        .map((row) => rowToStr2(row))
+        .join('\n')
+}
+</script>
