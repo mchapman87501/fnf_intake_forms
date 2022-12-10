be63497269a3d5fa944a211120c84158fc441bae
Mitch Chapman <mchapman87501@users.noreply.github.com>
2022-12-10 13:04:21 -0700

    Log owner surrender form parameters, as received, to 'npm run dev' output.

--- src/routes/api/v1/owner_surrender_form/+server.ts
+++ src/routes/api/v1/owner_surrender_form/+server.ts
@@ -4,8 +4,8 @@ import { json } from '@sveltejs/kit'
 export const prerender = false
 
 export async function POST(event: RequestEvent): Promise<Response> {
-    const params = event.params
-    const body = event.request.body
-    console.log("Pretend to handle owner surrender form with params: %o, body: %o", params, body)
+
+    let formParams: { [index: string]: any } = await event.request.json()
+    console.log("Received form data: %o", formParams)
     return json("Result\r\nPretend CSV content")
 }
