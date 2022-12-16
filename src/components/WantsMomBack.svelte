07ac45be97570396c4c03d79f4b4e0fa0e7673f3
bden <bkdenhartog@gmail.com>
2022-12-16 11:39:02 -0700

    Merge from main

---
+++ src/components/WantsMomBack.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { recvdFromPkg } from '../infrastructure/stores.js'
+
+</script>
+
+<hr />
+<label>
+	<input type="checkbox" bind:checked={$recvdFromPkg.wantsMomBack} />
+	Wants to re-adopt Mother Cat after kittens are weaned and she has been spayed
+</label>
+
+<style>
+	label {
+		font-size: 75%;
+	}
+</style>
