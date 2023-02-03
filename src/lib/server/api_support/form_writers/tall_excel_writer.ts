cfa2ab09e64a492bbf7340b288250c60892f7489
Mitch Chapman <mchapman87501@users.noreply.github.com>
2023-02-03 10:15:35 -0700

    Issue #17: in some env's importing exceljs is tricky, async.

--- src/lib/server/api_support/form_writers/tall_excel_writer.ts
+++ src/lib/server/api_support/form_writers/tall_excel_writer.ts
@@ -1,5 +1,5 @@
 // Write Excel files in a standard 3-column format.
-import * as exceljs from 'exceljs'
+
 import { getDownloadInfo } from '$lib/api_support/download_info'
 
 export interface Row {
@@ -14,7 +14,17 @@ export function row(name: string, value: string): Row {
 	return { name: name, value: value, comments: '' }
 }
 
+// exceljs has import problems in some build environments?
+// https://github.com/alexfauquette/mui-x/commit/4b9d769d2352b01a2de7c8957fc7d891ac0a5e92
+async function getExcelJS() {
+	const { default: excelJsDefault } = await import('exceljs')
+	return excelJsDefault
+}
+
+// import * as exceljs from 'exceljs'
+
 export async function writeFile(pathname: string, worksheetName: string, records: Row[]) {
+	const exceljs = await getExcelJS()
 	const wb = new exceljs.Workbook()
 	const sheet = wb.addWorksheet(worksheetName)
 	sheet.properties.defaultColWidth = 32
